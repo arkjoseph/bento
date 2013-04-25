fe0639fbd6a3bd574710bd5e7dd6e15601c189dc
Joseph DK <josephpgarrido@gmail.com>
2013-04-25 15:13:05 -0700

    st

--- sites/all/themes/basic/views/views-view-table--data-management.tpl.php
+++ sites/all/themes/basic/views/views-view-table--data-management.tpl.php
@@ -18,29 +18,30 @@
 <header>
 	<a class="add" href="/node/add/data-collection" title="Add a Title" data-target="#rEdit">+</a>
 </header>
-
-<table class="<?php print $class; ?>"<?php print $attributes; ?>>
-  <?php if (!empty($title)) : ?>
-    <caption><?php print $title; ?></caption>
-  <?php endif; ?>
-  <thead>
-    <tr>
-      <?php foreach ($header as $field => $label): ?>
-        <th class="views-field views-field-<?php print $fields[$field]; ?>">
-          <?php print $label; ?>
-        </th>
-      <?php endforeach; ?>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach ($rows as $count => $row): ?>
-      <tr class="<?php print implode(' ', $row_classes[$count]); ?>">
-        <?php foreach ($row as $field => $content): ?>
-          <td class="views-field views-field-<?php print $fields[$field]; ?>">
-            <?php print $content; ?>
-          </td>
-        <?php endforeach; ?>
-      </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
+<section>
+	<table class="<?php print $class; ?>"<?php print $attributes; ?>>
+	  <?php if (!empty($title)) : ?>
+	    <caption><?php print $title; ?></caption>
+	  <?php endif; ?>
+	  <thead>
+	    <tr>
+	      <?php foreach ($header as $field => $label): ?>
+	        <th class="views-field views-field-<?php print $fields[$field]; ?>">
+	          <?php print $label; ?>
+	        </th>
+	      <?php endforeach; ?>
+	    </tr>
+	  </thead>
+	  <tbody>
+	    <?php foreach ($rows as $count => $row): ?>
+	      <tr class="<?php print implode(' ', $row_classes[$count]); ?>">
+	        <?php foreach ($row as $field => $content): ?>
+	          <td class="views-field views-field-<?php print $fields[$field]; ?>">
+	            <?php print $content; ?>
+	          </td>
+	        <?php endforeach; ?>
+	      </tr>
+	    <?php endforeach; ?>
+	  </tbody>
+	</table>
+</section>
